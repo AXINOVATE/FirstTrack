6db69e1f8f547dffa29d2c6f00807bee5790f4d7
Raja <rajasekhar.kandula@axinovate.com>
2016-03-02 14:35:21 +0530

    Style css added

--- application/views/home/cars.php
+++ application/views/home/cars.php
@@ -9,8 +9,9 @@ $prefix=$this->config->item('prefix');
 	<meta charset="utf-8">
 	<title>NayaGaadi.com is India's first Online market place for all the Brand New Vehicles.|Book Online, Buy Online , Test Ride all New Autos, Bikes, Cycles, Cars, Scooters, Buses, Lorries, Tractors, Trucks, Passenger Vehicles, Commercial vehicles, Utility Vehicles, SUV's, MUV in India,  New Autos, Bikes, Cycles, Cars, Scooters, Buses, Lorries, Tractors, Trucks, Passenger Vehicles, Commercial vehicles, Utility Vehicles, SUV's, MUV at your Door Step.</title>
 	<link href="<?php echo $assetsPath;?>/css/bootstrap.min.css" type="text/css" rel="stylesheet">
-	<link href="<?php echo $assetsPath;?>/css/custom.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+	<link href="<?php echo $assetsPath;?>/css/style.css" type="text/css" rel="stylesheet">
+	<link href="<?php echo $assetsPath;?>/css/custom.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/images/favicon.png" rel="icon" />
 	<style type="text/css">
 
@@ -26,7 +27,14 @@ $prefix=$this->config->item('prefix');
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h2>Car Models</h2>
+					<h3 class="mt-0">Car Models</h3>
+				</div>
+				<div class="col-md-3">
+					<div>
+					</div>
+				</div>
+				<div class="col-md-9">
+					asd
 				</div>
 			</div>
 		</div>
