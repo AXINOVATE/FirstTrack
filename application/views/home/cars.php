c63408bdfc0530a54b63a9eed53979684276ab34
Raja <rajasekhar.kandula@axinovate.com>
2016-03-02 14:19:42 +0530

    css file chamges

--- application/views/home/cars.php
+++ application/views/home/cars.php
@@ -9,7 +9,7 @@ $prefix=$this->config->item('prefix');
 	<meta charset="utf-8">
 	<title>NayaGaadi.com is India's first Online market place for all the Brand New Vehicles.|Book Online, Buy Online , Test Ride all New Autos, Bikes, Cycles, Cars, Scooters, Buses, Lorries, Tractors, Trucks, Passenger Vehicles, Commercial vehicles, Utility Vehicles, SUV's, MUV in India,  New Autos, Bikes, Cycles, Cars, Scooters, Buses, Lorries, Tractors, Trucks, Passenger Vehicles, Commercial vehicles, Utility Vehicles, SUV's, MUV at your Door Step.</title>
 	<link href="<?php echo $assetsPath;?>/css/bootstrap.min.css" type="text/css" rel="stylesheet">
-	<link href="<?php echo $assetsPath;?>/css/home_page_style.css" type="text/css" rel="stylesheet">
+	<link href="<?php echo $assetsPath;?>/css/custom.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/css/font-awesome.min.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/images/favicon.png" rel="icon" />
 	<style type="text/css">
@@ -25,115 +25,8 @@ $prefix=$this->config->item('prefix');
 	<div class="body-container">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-3 col-sm-4 col-xs-12 border-lt-grey">
-					<div class="row">
-						<div class="col-md-4 col-sm-4 col-xs-4 cat-box bg-lightblue">
-							<div>
-								<center>
-									<img src="<?php echo $assetsPath;?>/images/sedan.png" class="cat-img" alt="car" />
-									<div style="font-size:14px; color:#fff; margin-bottom:10px;">Car</div>
-								</center>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-4 col-xs-4 cat-box bg-lightgrey">
-							
-							<div>
-								<center>
-									<img src="<?php echo $assetsPath;?>/images/bike.png" class="cat-img" alt="car">
-									<div style="font-size:14px; color:#696969; margin-bottom:10px;">Bike</div>
-								</center>
-							</div>
-						</div>
-						<div class="col-md-4 col-sm-4 col-xs-4 cat-box bg-lightgrey">
-							
-							<div>
-								<center>
-									<img src="<?php echo $assetsPath;?>/images/more1.png" class="cat-img" alt="car" />
-									<div style="font-size:14px; color:#696969; margin-bottom:10px;">More</div>
-								</center>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12 col-sm-12 col-xs-12 text-center">
-							<h5>Body Type</h5>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
-							<div class="border-lt-grey ml-m10 mr-m10">
-								<img src="<?php echo $assetsPath;?>/images/hatchback.png" class="cat-img" alt="car" />
-								<div class="car-type-name">Hatchback</div>
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
-							<div class="border-lt-grey ml-m10 mr-m10">
-								<img src="<?php echo $assetsPath;?>/images/sedan.png" class="cat-img" alt="car" />
-								<div class="car-type-name">Sedan</div>
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
-							<div class="border-lt-grey ml-m10 mr-m10">
-								<img src="<?php echo $assetsPath;?>/images/suv.png" class="cat-img" alt="car" />
-								<div class="car-type-name">SUV</div>
-							</div>
-						</div>
-						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
-							<div class="border-lt-grey ml-m10 mr-m10">
-								<img src="<?php echo $assetsPath;?>/images/muv.png" class="cat-img" alt="car" />
-								<div class="car-type-name">MUV</div>
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12 col-sm-12 col-xs-12 mt-10 mb-10">
-							<div class="mr-m5 ml-m5">
-								<a href="javascript:void(0)" class="search-btn" >Search</a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-9">
-				
-				</div>
-			</div>
-			
-			<div class="row mt-20">
-				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
-					<center>
-						<img src="<?php echo $assetsPath;?>/images/adv-booking.png" class="mt-10" width="50%" alt="car" />
-					</center>
-					<div class="car-type-name">Advance<br/>Booking</div>
-				</div>
-				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
-					<center>
-						<img src="<?php echo $assetsPath;?>/images/test-drive.png" class="mt-10" width="50%" alt="car" />
-					</center>
-					<div class="car-type-name">Request for<br/>Test Drive</div>
-				</div>
-				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
-					<center>
-						<img src="<?php echo $assetsPath;?>/images/vehicle-loan.png" class="mt-10" width="50%" alt="car" />
-					</center>
-					<div class="car-type-name">Apply for<br/>Vehicle Loan</div>
-				</div>
-				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
-					<center>
-						<img src="<?php echo $assetsPath;?>/images/apply-insurance.png" class="mt-10" width="50%" alt="car" />
-					</center>
-					<div class="car-type-name">Apply for<br/>Insurance</div>
-				</div>
-				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
-					<center>
-						<img src="<?php echo $assetsPath;?>/images/cost-analyzer.png" class="mt-10" width="50%" alt="car" />
-					</center>
-					<div class="car-type-name">Service Cost<br/>Analyzer</div>
-				</div>
-				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
-					<center>
-						<img src="<?php echo $assetsPath;?>/images/locate-dealer.png" class="mt-10" width="50%" alt="car" />
-					</center>
-					<div class="car-type-name">Locate a <br/>Dealer</div>
+				<div class="col-md-12">
+					<h2>Car Models</h2>
 				</div>
 			</div>
 		</div>
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -7,10 +7,11 @@ $prefix=$this->config->item('prefix');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome First Track</title>
+	<title>NayaGaadi.com is India's first Online market place for all the Brand New Vehicles.|Book Online, Buy Online , Test Ride all New Autos, Bikes, Cycles, Cars, Scooters, Buses, Lorries, Tractors, Trucks, Passenger Vehicles, Commercial vehicles, Utility Vehicles, SUV's, MUV in India,  New Autos, Bikes, Cycles, Cars, Scooters, Buses, Lorries, Tractors, Trucks, Passenger Vehicles, Commercial vehicles, Utility Vehicles, SUV's, MUV at your Door Step.</title>
 	<link href="<?php echo $assetsPath;?>/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/css/custom.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+	<link href="<?php echo $assetsPath;?>/images/favicon.png" rel="icon" />
 	<style type="text/css">
 
 	</style>
@@ -24,63 +25,117 @@ $prefix=$this->config->item('prefix');
 	<div class="body-container">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-4 border-all-lite ">
-					<div class="categories-list">
-						<div class="row">
-							<div class="col-md-4 col-sm-4 col-xs-4 img-box-1">
-								<div class="categori-box">
-									<div>
-									<center>
-									<img src="<?php echo $assetsPath;?>/images/car1.png" class="" alt="car" />
-									<div><center><h6>Car</h6></center></div>
-									</center>
-									</div>
-								</div>
+				<div class="col-md-3 col-sm-4 col-xs-12 border-lt-grey">
+					<div class="row">
+						<div class="col-md-4 col-sm-4 col-xs-4 cat-box bg-lightblue">
+							<div>
+								<center>
+									<img src="<?php echo $assetsPath;?>/images/sedan.png" class="cat-img" alt="car" />
+									<div style="font-size:14px; color:#fff; margin-bottom:10px;">Car</div>
+								</center>
 							</div>
-							<div class="col-md-4 col-sm-4 col-xs-4 img-box-all">
-								<div class="categori-box">
-									<div>
-									<center>
-									<img src="<?php echo $assetsPath;?>/images/bike1.png" class="" alt="car">
-									<div><center><h6>Bike</h6></center></div>
-									</center>
-									</div>
-								</div>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-4 cat-box bg-lightgrey">
+							
+							<div>
+								<center>
+									<img src="<?php echo $assetsPath;?>/images/bike.png" class="cat-img" alt="car">
+									<div style="font-size:14px; color:#696969; margin-bottom:10px;">Bike</div>
+								</center>
 							</div>
-							<div class="col-md-4 col-sm-4 col-xs-4 img-box-all">
-								<div class="categori-box">
-									<div>
-									<center>
-									<img src="<?php echo $assetsPath;?>/images/more1.png" class="" alt="car" />
-									<div><center><h6>More</h6></center></div>
-									</center>
-									</div>
-								</div>
+						</div>
+						<div class="col-md-4 col-sm-4 col-xs-4 cat-box bg-lightgrey">
+							
+							<div>
+								<center>
+									<img src="<?php echo $assetsPath;?>/images/more1.png" class="cat-img" alt="car" />
+									<div style="font-size:14px; color:#696969; margin-bottom:10px;">More</div>
+								</center>
 							</div>
 						</div>
 					</div>
-					<h4>Body Type</h4>
-					<div class="col-md-6 border-all-lite">
-						<img src="<?php echo $assetsPath;?>/images/car1.png" class="" alt="car" />
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12 text-center">
+							<h5>Body Type</h5>
+						</div>
 					</div>
-					
-					<div class="col-md-6 border-all-lite">
-						<img src="<?php echo $assetsPath;?>/images/car1.png" class="" alt="car" />
+					<div class="row">
+						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
+							<div class="border-lt-grey ml-m10 mr-m10">
+								<img src="<?php echo $assetsPath;?>/images/hatchback.png" class="cat-img" alt="car" />
+								<div class="car-type-name">Hatchback</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
+							<div class="border-lt-grey ml-m10 mr-m10">
+								<img src="<?php echo $assetsPath;?>/images/sedan.png" class="cat-img" alt="car" />
+								<div class="car-type-name">Sedan</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
+							<div class="border-lt-grey ml-m10 mr-m10">
+								<img src="<?php echo $assetsPath;?>/images/suv.png" class="cat-img" alt="car" />
+								<div class="car-type-name">SUV</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6 col-xs-6 mb-10">
+							<div class="border-lt-grey ml-m10 mr-m10">
+								<img src="<?php echo $assetsPath;?>/images/muv.png" class="cat-img" alt="car" />
+								<div class="car-type-name">MUV</div>
+							</div>
+						</div>
 					</div>
-					
+					<div class="row">
+						<div class="col-md-12 col-sm-12 col-xs-12 mt-10 mb-10">
+							<div class="mr-m5 ml-m5">
+								<a href="javascript:void(0)" class="search-btn" >Search</a>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-8">
-				sadasdasasd
-				asd
-				asdasd
-				asdasd</br>
-				asdasd</br>
-				asdasd</br>
-				asdasd</br>
-				asdasd</br>
+				<div class="col-md-9">
+				
 				</div>
 			</div>
 			
+			<div class="row mt-20">
+				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
+					<center>
+						<img src="<?php echo $assetsPath;?>/images/adv-booking.png" class="mt-10" width="50%" alt="car" />
+					</center>
+					<div class="car-type-name">Advance<br/>Booking</div>
+				</div>
+				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
+					<center>
+						<img src="<?php echo $assetsPath;?>/images/test-drive.png" class="mt-10" width="50%" alt="car" />
+					</center>
+					<div class="car-type-name">Request for<br/>Test Drive</div>
+				</div>
+				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
+					<center>
+						<img src="<?php echo $assetsPath;?>/images/vehicle-loan.png" class="mt-10" width="50%" alt="car" />
+					</center>
+					<div class="car-type-name">Apply for<br/>Vehicle Loan</div>
+				</div>
+				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
+					<center>
+						<img src="<?php echo $assetsPath;?>/images/apply-insurance.png" class="mt-10" width="50%" alt="car" />
+					</center>
+					<div class="car-type-name">Apply for<br/>Insurance</div>
+				</div>
+				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
+					<center>
+						<img src="<?php echo $assetsPath;?>/images/cost-analyzer.png" class="mt-10" width="50%" alt="car" />
+					</center>
+					<div class="car-type-name">Service Cost<br/>Analyzer</div>
+				</div>
+				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
+					<center>
+						<img src="<?php echo $assetsPath;?>/images/locate-dealer.png" class="mt-10" width="50%" alt="car" />
+					</center>
+					<div class="car-type-name">Locate a <br/>Dealer</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<!-- Body content ends here -->	
