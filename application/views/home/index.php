efb35a2504450ab97e50cff472aea7940e60a79e
Kirtiman <kirtiman.patowary@axinovate.com>
2016-03-03 20:06:04 +0530

    homepage popup added

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -14,8 +14,10 @@ $prefix=$this->config->item('prefix');
 	<link href="<?php echo $assetsPath;?>/css/font-awesome.min.css" type="text/css" rel="stylesheet">
 	<link href="<?php echo $assetsPath;?>/images/favicon.png" rel="icon" />
 	<style type="text/css">
-		
-	
+		#adv-book-modal .modal-header{background-color: #0a3151; color: white !important;}
+		#adv-book-modal .modal-header .close{color: white !important;}
+		#adv-book-modal .modal-body{background-color: #027cd5; color: white !important; overflow:auto;}
+		#adv-book-modal .modal-footer{background-color: #027cd5; color: white !important;}
 	</style>
 </head>
 <body>
@@ -98,72 +100,72 @@ $prefix=$this->config->item('prefix');
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>
 									<img src="<?php echo $assetsPath;?>/images/scooters.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Scooters</div>
 								</center>
-								<div class="car-type-name">Scooters</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-yellow pd-5">
 								<center>
-									<img src="<?php echo $assetsPath;?>/images/tractors.png" style="width:70%;" alt="car" />	
+									<img src="<?php echo $assetsPath;?>/images/tractors.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Tractors</div>	
 								</center>
-								<div class="car-type-name">Tractors</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>	
 									<img src="<?php echo $assetsPath;?>/images/auto.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Auto</div>
 								</center>
-								<div class="car-type-name">Auto</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>
 									<img src="<?php echo $assetsPath;?>/images/lorries.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Lorries</div>
 								</center>
-								<div class="car-type-name">Lorries</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>
 									<img src="<?php echo $assetsPath;?>/images/buses.png" style="width:70%;" alt="car" />	
+									<div class="car-type-name">Buses</div>
 								</center>
-								<div class="car-type-name">Buses</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>	
 									<img src="<?php echo $assetsPath;?>/images/ampere.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Ampere</div>
 								</center>
-								<div class="car-type-name">Ampere</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>
 									<img src="<?php echo $assetsPath;?>/images/birdgroup.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Bird Group</div>
 								</center>
-								<div class="car-type-name">Bird Group</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>
 									<img src="<?php echo $assetsPath;?>/images/fivewheels.png" style="width:70%;" alt="car" />	
+									<div class="car-type-name">Five Wheels</div>
 								</center>
-								<div class="car-type-name">Five Wheels</div>
 							</div>
 						</div>
 						<div class="col-md-4 col-sm-4 col-xs-4 mb-10">
 							<div class="border-lt-grey ml-m10 mr-m10 bg-lightgrey pd-5">
 								<center>	
 									<img src="<?php echo $assetsPath;?>/images/segways.png" style="width:70%;" alt="car" />
+									<div class="car-type-name">Segway</div>
 								</center>
-								<div class="car-type-name">Segway</div>
 							</div>
 						</div>
 					</div>
@@ -180,23 +182,23 @@ $prefix=$this->config->item('prefix');
 						<div class="tab-pane fade in active" id="tab1">
 							<a href="<?php echo $prefix;?>/home/news_detail">
 							<center>
-								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10" width="70%" alt="car" />
+								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10 banner-img" alt="car" />
 							</center>
 							</a>
 						</div>
 						<div class="tab-pane fade" id="tab2">
 							<center>
-								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10" width="70%" alt="car" />
+								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10 banner-img" alt="car" />
 							</center>
 						</div>
 						<div class="tab-pane fade" id="tab3">
 							<center>
-								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10" width="70%" alt="car" />
+								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10 banner-img" alt="car" />
 							</center>
 						</div>
 						<div class="tab-pane fade" id="tab4">
 							<center>
-								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10" width="70%" alt="car" />
+								<img src="<?php echo $assetsPath;?>/images/baner-car.png" class="mt-10 banner-img" alt="car" />
 							</center>
 						</div>
 					</div>
@@ -211,47 +213,133 @@ $prefix=$this->config->item('prefix');
 			</div>
 			
 			<div class="row mt-20">
+				<a href ="javascript:void(0)" data-toggle="modal" data-target="#adv-book-modal">
 				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
 					<center>
 						<img src="<?php echo $assetsPath;?>/images/adv-booking.png" class="mt-10" width="50%" alt="car" />
+						<div class="car-type-name">Advance<br/>Booking</div>
 					</center>
-					<div class="car-type-name">Advance<br/>Booking</div>
 				</div>
+				</a>
 				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
 					<center>
 						<img src="<?php echo $assetsPath;?>/images/test-drive.png" class="mt-10" width="50%" alt="car" />
+						<div class="car-type-name">Request for<br/>Test Drive</div>
 					</center>
-					<div class="car-type-name">Request for<br/>Test Drive</div>
 				</div>
 				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
 					<center>
 						<img src="<?php echo $assetsPath;?>/images/vehicle-loan.png" class="mt-10" width="50%" alt="car" />
+						<div class="car-type-name">Apply for<br/>Vehicle Loan</div>
 					</center>
-					<div class="car-type-name">Apply for<br/>Vehicle Loan</div>
 				</div>
 				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
 					<center>
 						<img src="<?php echo $assetsPath;?>/images/apply-insurance.png" class="mt-10" width="50%" alt="car" />
+						<div class="car-type-name">Apply for<br/>Insurance</div>
 					</center>
-					<div class="car-type-name">Apply for<br/>Insurance</div>
 				</div>
 				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
 					<center>
 						<img src="<?php echo $assetsPath;?>/images/cost-analyzer.png" class="mt-10" width="50%" alt="car" />
+						<div class="car-type-name">Service Cost<br/>Analyzer</div>
 					</center>
-					<div class="car-type-name">Service Cost<br/>Analyzer</div>
 				</div>
 				<div class="col-md-2 col-sm-2 col-xs-4 border-lt-blue mb-10">
 					<center>
 						<img src="<?php echo $assetsPath;?>/images/locate-dealer.png" class="mt-10" width="50%" alt="car" />
+						<div class="car-type-name">Locate a <br/>Dealer</div>
 					</center>
-					<div class="car-type-name">Locate a <br/>Dealer</div>
 				</div>
 			</div>
 		</div>
 	</div>
 	<!-- Body content ends here -->	
+
 	
+	
+	
+	<!-- Modal starts here -->
+	<div class="modal fade" id="adv-book-modal" role="dialog">
+		<div class="modal-dialog modal-md">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="text-center modal-title">Request for Test Drive</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label for="fullName" class="col-md-3 col-sm-3 col-xs-12 control-label">Full Name</label>
+						<div class="col-md-9 col-sm-9 col-xs-12 mb-10">
+							<input type="text" class="form-control" id="fullName" name="fullName" placeholder="Full Name" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="phone" class="col-md-3 col-sm-3 col-xs-12 control-label">Phone</label>
+						<div class="col-md-9 col-sm-9 col-xs-12 mb-10">
+							<input type="text" class="form-control" id="phone" name="phone" placeholder="eg. 9874563110" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="emailID" class="col-md-3 col-sm-3 col-xs-12 control-label">Email-id</label>
+						<div class="col-md-9 col-sm-9 col-xs-12 mb-10">
+							<input type="text" class="form-control" id="emailID" name="emailID" placeholder="tony@gmail.com" />
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="city" class="col-md-3 col-sm-3 col-xs-12 control-label">City</label>
+						<div class="col-md-9 col-sm-9 col-xs-12">
+							<select class="form-control entity-type select2 mb-10" id="city" style="width:100%;">
+								<option value="">-- Select City --</option>
+								<option value="1" >Bangalore</option>
+								<option value="1" >Mysore</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="maker" class="col-md-3 col-sm-3 col-xs-12 control-label">Maker</label>
+						<div class="col-md-9 col-sm-9 col-xs-12">
+							<select class="form-control entity-type select2 mb-10" id="maker" style="width:100%;">
+								<option value="">-- Select Maker --</option>
+								<option value="1" >Maruti Suzuki</option>
+								<option value="1" >Hyundai</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="model" class="col-md-3 col-sm-3 col-xs-12 control-label">Model</label>
+						<div class="col-md-9 col-sm-9 col-xs-12">
+							<select class="form-control entity-type select2 mb-10" id="model" style="width:100%;">
+								<option value="">-- Select Model --</option>
+								<option value="1" >Swift</option>
+								<option value="1" >I20</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="variant" class="col-md-3 col-sm-3 col-xs-12 control-label">Variant</label>
+						<div class="col-md-9 col-sm-9 col-xs-12">
+							<select class="form-control entity-type select2 mb-10" id="variant" style="width:100%;">
+								<option value="">-- Select Variant --</option>
+								<option value="1" >Petrol</option>
+								<option value="1" >Diesel</option>
+							</select>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="" class="col-md-4 col-sm-4 hidden-xs"> &nbsp; </label>
+						<div class="col-md-4 col-sm-4 col-xs-12">
+							<a href="javascript:void(0)" class="search-btn" style="background-color:#F9D133;" >Save</a>
+							<!--button type="button" class="btn btn-default" data-dismiss="modal">Close</button-->
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+  <!-- Modal ends here -->
+  
 <script src="<?php echo $assetsPath; ?>/js/jquery-1.12.1.min.js"></script>
 <script src="<?php echo $assetsPath; ?>/js/bootstrap.min.js" type="text/javascript"></script>
 <script>
